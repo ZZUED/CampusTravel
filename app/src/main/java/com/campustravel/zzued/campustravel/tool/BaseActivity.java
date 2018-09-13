18e3b25fb2a228cd51b82be6e99969c742f37aba
EricJeffrey <1719937412@qq.com>
2018-09-14 00:33:36 +0800

    ericjeffrey add splash and loginRegister layout

---
+++ app/src/main/java/com/campustravel/zzued/campustravel/tool/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.campustravel.zzued.campustravel.tool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.add(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.remove(this);
+    }
+}
