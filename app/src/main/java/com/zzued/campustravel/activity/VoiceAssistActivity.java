5c31b0195d4bf03a6212d63aed15f20f9d2ae500
EricJeffrey <1719937412@qq.com>
2018-10-26 10:03:07 +0800

    语音助手现在可以识别介绍图书馆之类的话了（仅仅字符串匹配）

--- app/src/main/java/com/zzued/campustravel/activity/VoiceAssistActivity.java
+++ app/src/main/java/com/zzued/campustravel/activity/VoiceAssistActivity.java
@@ -26,6 +26,9 @@ import com.zzued.campustravel.util.TextToSpeech;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
+import java.util.List;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -50,7 +53,7 @@ public class VoiceAssistActivity extends BaseActivity {
         public boolean handleMessage(Message msg) {
             switch (msg.what) {
                 case 1:
-                    String str = (String)msg.obj;
+                    String str = (String) msg.obj;
                     startPlay(str);
                     break;
                 default:
@@ -97,13 +100,13 @@ public class VoiceAssistActivity extends BaseActivity {
                         if (i == 0)
                             try {
                                 JSONObject jsonObject = new JSONObject(recognizedResult.asr_out);
-                                String tmp = jsonObject.getString("result");
+                                String tmp = extractHotWord(jsonObject.getString("result"));
                                 recognizedText.setText(tmp);
                                 sendRecorder(tmp);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-                        else{
+                        else {
                             Log.e(TAG, "onRecognizingResult: error: " + i);
                             recognizedText.setText("识别出错");
                         }
@@ -120,11 +123,25 @@ public class VoiceAssistActivity extends BaseActivity {
     }
 
     /**
+     * 判断目标字符串中是否包含经典列表
+     *
+     * @param target 目标字符串
+     * @return 包含的景点列表字符串或原目标传
+     */
+    private String extractHotWord(String target) {
+        List<String> scenicSpots = Arrays.asList("泊月路", "亭云路", "春华路", "秋实路", "湖滨路", "仁和大道", "天健大道", "厚德大道", "毓秀路", "大学生活动中心", "行政楼", "教学区南区", "教学区北区", "菊园快递收发中心", "启明广场", "体育馆", "图书馆", "五星广场", "元和广场", "郑州大学校医院", "钟楼", "博雅湖（眉湖）", "厚山", "眉湖九博（河源）", "眉湖九博（问鼎）", "眉湖九博（博弈）", "眉湖九博（禅趣）", "眉湖九博（太和柱，和天烛）", "眉湖九博（观星测影）", "眉湖九博（大道通渠）", "眉湖九博（凤台荷香）", "樱花林", "丁老头奶酪（郑大店）", "京东便利店（原校园风超市）", "蜜雪冰城（荷园商业街南侧）", "沁园春超市", "匆匆那年餐厅", "荷园二餐厅", "荷园一餐厅", "正味居", "菊园秋穗园餐厅", "柳园风华园餐厅", "柳园聚英园餐厅", "柳园清真餐厅", "柳园同和昌食府餐厅", "松园竞舸园餐厅", "松园嵩阳食府餐厅", "荷园宿舍区", "菊园宿舍区", "柳园宿舍区", "松园宿舍区", "材料科学与工程学院", "电气工程学院", "管理工程学院", "化学与能源学院", "化学与分子工程学院", "机械工程学院", "建筑学院", "力学与工程学院", "生命科学学院", "数学与统计学院", "水利与环境学院", "土木工程学院", "物理工程学院", "法学院（知识产权学院）", "公共管理学院", "教育学院", "历史学院", "旅游管理学院", "马克思主义学院", "美术学院", "商学院", "体育学院（校本部）", "外语学院", "文学院", "新闻与传播学院", "信息管理学院", "公共卫生学院", "护理学院", "基础医学院", "药学院", "产业技术研究院（大学科技园）", "国际教育学院", "河南省资源与材料工业技术研究院（以下简称研究院）", "信息工程学院");
+        for (String sample : scenicSpots)
+            if (target.contains(sample))
+                return sample;
+        return target;
+    }
+
+    /**
      * 用于发送识别出来的文本
      *
      * @param recorder 识别出来的文本
      */
-    public void sendRecorder(String recorder){
+    public void sendRecorder(String recorder) {
         final String recorder_now = recorder;
         new Thread(new Runnable() {
             @Override
